3937c0d633b3836197074e1bb99d33e9f2a461eb
Chris Hale <design@chrishale.co.uk>
2012-10-10 18:16:50 +0100

    configure method

--- lib/shopify_theme.rb
+++ lib/shopify_theme.rb
@@ -4,17 +4,17 @@ module ShopifyTheme
 
   NOOPParser = Proc.new {|data, format| {} }
 
-  def self.asset_list
+  def self.asset_list(env)
     # HTTParty parser chokes on assest listing, have it noop
     # and then use a rel JSON parser.
-    response = shopify.get(path, :parser => NOOPParser)
+    response = shopify(env).get(path(env), :parser => NOOPParser)
     assets = JSON.parse(response.body)["assets"].collect {|a| a['key'] }
     # Remove any .css files if a .css.liquid file exists
     assets.reject{|a| assets.include?("#{a}.liquid") }
   end
 
-  def self.get_asset(asset)
-    response = shopify.get(path, :query =>{:asset => {:key => asset}}, :parser => NOOPParser)
+  def self.get_asset(asset, env)
+    response = shopify(env).get(path(env), :query =>{:asset => {:key => asset}}, :parser => NOOPParser)
     # HTTParty json parsing is broken?
     JSON.parse(response.body)["asset"]
   end
@@ -23,7 +23,7 @@ module ShopifyTheme
     shopify(env).put(path(env), :body =>{:asset => data})
   end
 
-  def self.delete_asset(asset)
+  def self.delete_asset(asset, env)
     shopify.delete(path, :body =>{:asset => {:key => asset}})
   end
 
@@ -32,11 +32,12 @@ module ShopifyTheme
   end
 
   def self.path(env)
-    @path ||= config[:theme_id] ? "/admin/themes/#{config[:"#{env}"][:theme_id]}/assets.json" : "/admin/assets.json" 
+    @path ||= config[:"#{env}"][:theme_id] ? "/admin/themes/#{config[:"#{env}"][:theme_id]}/assets.json" : "/admin/assets.json" 
   end
 
-  def self.ignore_files
-    @ignore_files ||= (config[:ignore_files] || []).compact.collect { |r| Regexp.new(r) }
+  def self.ignore_files(env)
+    files_array = config[:"#{env}"][:ignore_files] || config[:ignore_files] || []
+    @ignore_files ||= files_array.compact.collect { |r| Regexp.new(r) }
   end
 
   def self.is_binary_data?(string)
--- lib/shopify_theme/cli.rb
+++ lib/shopify_theme/cli.rb
@@ -19,18 +19,19 @@ module ShopifyTheme
     end
 
     desc "configure API_KEY PASSWORD STORE THEME_ID", "generate a config file for the store to connect to"
-    def configure(api_key=nil, password=nil, store=nil, theme_id=nil)
-      config = {:api_key => api_key, :password => password, :store => store, :theme_id => theme_id, :ignore_files => ["README"]}
+    def configure(api_key=nil, password=nil, store=nil, theme_id=nil, env='development')
+      config = {:"#{env}" => {:api_key => api_key, :password => password, :store => store, :theme_id => theme_id, :ignore_files => ["README"]}}
       create_file('config.yml', config.to_yaml)
     end
 
     desc "download FILE", "download the shops current theme assets"
     method_option :quiet, :type => :boolean, :default => false
+    method_option :env, :type => :string, :default => 'development'
     def download(*keys)
-      assets = keys.empty? ? ShopifyTheme.asset_list : keys
+      assets = keys.empty? ? ShopifyTheme.asset_list(env) : keys
 
       assets.each do |asset|
-        download_asset(asset)
+        download_asset(asset, options['env'])
         say("Downloaded: #{asset}", :green) unless options['quiet']
       end
       say("Done.", :green) unless options['quiet']
@@ -38,26 +39,28 @@ module ShopifyTheme
 
     desc "upload FILE", "upload all theme assets to shop"
     method_option :quiet, :type => :boolean, :default => false
+    method_option :env, :type => :string, :default => 'development'
     def upload(*keys)
       assets = keys.empty? ? local_assets_list : keys
       assets.each do |asset|
-        send_asset(asset, options['quiet'])
+        send_asset(asset, options['quiet'], options['env'])
       end
       say("Done.", :green) unless options['quiet']
     end
 
     desc "replace FILE", "completely replace shop theme assets with local theme assets"
     method_option :quiet, :type => :boolean, :default => false
+    method_option :env, :type => :string, :default => 'development'
     def replace(*keys)
       say("Are you sure you want to completely replace your shop theme assets? This is not undoable.", :yellow)
       if ask("Continue? (Y/N): ") == "Y"
-        remote_assets = keys.empty? ? ShopifyTheme.asset_list : keys
+        remote_assets = keys.empty? ? ShopifyTheme.asset_list(env) : keys
         remote_assets.each do |asset|
-          delete_asset(asset, options['quiet'])
+          delete_asset(asset, options['quiet'], options['env'])
         end
         local_assets = keys.empty? ? local_assets_list : keys
         local_assets.each do |asset|
-          send_asset(asset, options['quiet'])
+          send_asset(asset, options['quiet'], options['env'])
         end
         say("Done.", :green) unless options['quiet']
       end
@@ -65,9 +68,10 @@ module ShopifyTheme
 
     desc "remove FILE", "remove theme asset"
     method_option :quiet, :type => :boolean, :default => false
+    method_option :env, :type => :string, :default => 'development'
     def remove(*keys)
       keys.each do |key|
-        delete_asset(key, options['quiet'])
+        delete_asset(key, options['quiet'], options['env'])
       end
       say("Done.", :green) unless options['quiet']
     end
@@ -103,8 +107,8 @@ module ShopifyTheme
       end
     end
 
-    def download_asset(key)
-      asset = ShopifyTheme.get_asset(key)
+    def download_asset(key, env)
+      asset = ShopifyTheme.get_asset(key, env)
       if asset['value']
         # For CRLF line endings
         content = asset['value'].gsub("\r", "")
@@ -116,7 +120,7 @@ module ShopifyTheme
       File.open(key, "w") {|f| f.write content} if content
     end
 
-    def send_asset(asset, quiet=false, env='development')
+    def send_asset(asset, quiet=false, env)
       data = {:key => asset}
       content = File.read(asset)
       if ShopifyTheme.is_binary_data?(content) || BINARY_EXTENSIONS.include?(File.extname(asset).gsub('.',''))
@@ -132,8 +136,8 @@ module ShopifyTheme
       end
     end
 
-    def delete_asset(key, quiet=false)
-      if (response = ShopifyTheme.delete_asset(key)).success?
+    def delete_asset(key, quiet=false, env)
+      if (response = ShopifyTheme.delete_asset(key, env)).success?
         say("Removed: #{key}", :green) unless quiet
       else
         say("Error: Could not remove #{key}. #{errors_from_response(response)}", :red)
